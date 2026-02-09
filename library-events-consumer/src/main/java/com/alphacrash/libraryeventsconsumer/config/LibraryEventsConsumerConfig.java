c53c975a54e35fd3ecd2fa28f42b39de3427cddd
alphacrash <sudhansu.ace@gmail.com>
2026-02-09 23:48:21 +0530

    Kafka Consumers

---
+++ library-events-consumer/src/main/java/com/alphacrash/libraryeventsconsumer/config/LibraryEventsConsumerConfig.java
@@ -0,0 +1,8 @@
+package com.alphacrash.libraryeventsconsumer.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class LibraryEventsConsumerConfig {
+
+}
