951531f2911ac521da485f56e9abd55cd8c4b18f
alphacrash <sudhansu.ace@gmail.com>
2026-02-09 21:43:08 +0530

    Kafka Producer Changes

--- library-events-producer/src/main/java/com/alphacrash/libraryeventsproducer/controller/LibraryEventsController.java
+++ library-events-producer/src/main/java/com/alphacrash/libraryeventsproducer/controller/LibraryEventsController.java
@@ -1,23 +1,47 @@
 package com.alphacrash.libraryeventsproducer.controller;
 
 import com.alphacrash.libraryeventsproducer.domain.LibraryEvent;
-import lombok.extern.slf4j.Slf4j;
+import com.alphacrash.libraryeventsproducer.domain.LibraryEventType;
+import com.alphacrash.libraryeventsproducer.producer.LibraryEventsProducer;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.ExecutionException;
+
 @RestController
-@Slf4j
 public class LibraryEventsController {
+
+    private final LibraryEventsProducer libraryEventsProducer;
+
+    public LibraryEventsController(LibraryEventsProducer libraryEventsProducer) {
+        this.libraryEventsProducer = libraryEventsProducer;
+    }
 
     @PostMapping("/v1/libraryevent")
     public ResponseEntity<LibraryEvent> postLibraryEvent(
             @RequestBody LibraryEvent libraryEvent
     ) {
-        log.info("Library event received: {}", libraryEvent);
-        // Invoke the Kafka Producer
+        libraryEventsProducer.sendLibraryEvent_Asynchronous(libraryEvent);
         return ResponseEntity.status(HttpStatus.CREATED).body(libraryEvent);
+    }
+
+    @PutMapping("/v1/libraryevent")
+    public ResponseEntity<?> putLibraryEvent(
+            @RequestBody @Valid LibraryEvent libraryEvent
+    ) throws ExecutionException, InterruptedException {
+        if (libraryEvent.libraryEventId() == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Please pass the libraryEventId");
+        }
+        if (!libraryEvent.libraryEventType().equals(LibraryEventType.UPDATE)) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Only UPDATE events are supported");
+        }
+
+        libraryEventsProducer.sendLibraryEvent_RecordWithHeaders(libraryEvent);
+        return ResponseEntity.status(HttpStatus.OK).body(libraryEvent);
     }
 }
---
+++ library-events-producer/src/main/java/com/alphacrash/libraryeventsproducer/producer/LibraryEventsProducer.java
@@ -0,0 +1,90 @@
+package com.alphacrash.libraryeventsproducer.producer;
+
+import com.alphacrash.libraryeventsproducer.domain.LibraryEvent;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.header.Header;
+import org.apache.kafka.common.header.internals.RecordHeader;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.stereotype.Component;
+import tools.jackson.databind.ObjectMapper;
+
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+@Component
+@Slf4j
+public class LibraryEventsProducer {
+
+    @Value("${spring.kafka.topic}")
+    public String topic;
+
+    private final KafkaTemplate<Integer, String> kafkaTemplate;
+    private final ObjectMapper objectMapper;
+
+    public LibraryEventsProducer(KafkaTemplate<Integer, String> kafkaTemplate, ObjectMapper objectMapper) {
+        this.kafkaTemplate = kafkaTemplate;
+        this.objectMapper = objectMapper;
+    }
+
+    public CompletableFuture<SendResult<Integer, String>> sendLibraryEvent_Asynchronous(LibraryEvent libraryEvent) {
+        var key = libraryEvent.libraryEventId();
+        var value = objectMapper.writeValueAsString(libraryEvent);
+
+        // 1. blocking call - get metadata about the kafka cluster
+        // 2. send message happens - returns a CompletableFuture
+        var completableFuture = kafkaTemplate.send(topic, key, value);
+
+        return completableFuture
+                .whenComplete((sendResult, throwable) -> {
+                   if (throwable != null) {
+                       handleFailure(key, value, throwable);
+                   } else {
+                       handleSuccess(key, value, sendResult);
+                   }
+                });
+    }
+
+    public SendResult<Integer, String> sendLibraryEvent_Synchronous(LibraryEvent libraryEvent) throws ExecutionException, InterruptedException, TimeoutException {
+        var key = libraryEvent.libraryEventId();
+        var value = objectMapper.writeValueAsString(libraryEvent);
+
+        // 1. blocking call - get metadata about the kafka cluster
+        // 2. block and wait until message is sent to kafka
+        var sendResult = kafkaTemplate.send(topic, key, value).get(3, TimeUnit.SECONDS);
+        handleSuccess(key, value, sendResult);
+        return sendResult;
+    }
+
+    public SendResult<Integer, String> sendLibraryEvent_RecordWithHeaders(LibraryEvent libraryEvent) throws ExecutionException, InterruptedException {
+        var key = libraryEvent.libraryEventId();
+        var value = objectMapper.writeValueAsString(libraryEvent);
+
+        var producerRecord = buildProducerRecord(key, value);
+        // 1. blocking call - get metadata about the kafka cluster
+        // 2. block and wait until message is sent to kafka
+        var sendResult = kafkaTemplate.send(producerRecord).get();
+        handleSuccess(key, value, sendResult);
+        return sendResult;
+    }
+
+    private ProducerRecord<Integer, String> buildProducerRecord(Integer key, String value) {
+        List<Header> recordHeaders = List.of(
+                new RecordHeader("event-source", "scanner".getBytes())
+        );
+        return new ProducerRecord<>(topic, null, key, value, recordHeaders);
+    }
+
+    private void handleSuccess(Integer key, String value, SendResult<Integer, String> sendResult) {
+        log.info("Message sent successfully for the key : {} and the value : {}, partition is {}", key, value, sendResult.getRecordMetadata().partition());
+    }
+
+    private void handleFailure(Integer key, String value, Throwable ex) {
+        log.error("Error sending the message and the exceptions is {}", ex.getMessage(), ex);
+    }
+}
